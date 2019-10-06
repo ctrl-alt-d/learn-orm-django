6ff514e7f170764cef6ed7ed1aa023800c3994fc
dani herrera <ctrl.alt.d@gmail.com>
2019-10-06 19:45:51 +0200

    Adding admin

--- src/facturplus/productes/admin.py
+++ src/facturplus/productes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Categoria, SubCategoria, Producte
+
+@admin.register(Producte)
+class ProducteAdmin(admin.ModelAdmin):
+    search_fields = ('nom', 'flags__nom')
+    list_filter = ('sub_categoria__categoria',)
+
+class ChoiceInline(admin.StackedInline):
+    model = SubCategoria
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(SubCategoria)
+
